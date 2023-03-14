5fbb5635a8960195979744d2132b3d58fb987331
NiklasRestory <niklas.cullberg@restory.ai>
2023-03-14 11:07:12 +0100

    Wrote flyweight and command descriptions

--- Lektion9Mars14DesignPatterns1/Command/ICommand.cs
+++ Lektion9Mars14DesignPatterns1/Command/ICommand.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace Lektion9Mars14DesignPatterns1.Command
 {
+    // The idea of the Command Pattern is that, when we have a button
+    // or something else that can do various different things when
+    // called, contain a Command object that implements the Command
+    // interface within it. This command class then contains its
+    // own function inside of its execute function.
+
+    // The structure allows us to invent new commands by just adding
+    // more command-classes that implement ICommand, and all the
+    // existing code works fine.
     public interface ICommand
     {
         void Execute();
--- Lektion9Mars14DesignPatterns1/Flyweight/AuthorFactory.cs
+++ Lektion9Mars14DesignPatterns1/Flyweight/AuthorFactory.cs
@@ -9,6 +9,14 @@ namespace Lektion9Mars14DesignPatterns1.Flyweight
     public class AuthorFactory
     {
         public Dictionary<string, Author> authors = new Dictionary<string, Author>();
+
+        // The idea of the Flyweight pattern is to reuse objects that have already
+        // been used. Hence, simply keep a dictionary containing the unique identifier
+        // for the various objects as a key, and if one comes in with the same
+        // identifier then get the already existing object. Otherwise, make a new one.
+        // Notably, this makes it so if you change the values of one Author, in
+        // this case, this value will be changed for all objects with that 
+        // particular author. They're all using the same object, after all.
         public Author GetAuthor(string Name, int Age, int NumberOfPrisesWon)
         {
             if (authors.ContainsKey(Name))
